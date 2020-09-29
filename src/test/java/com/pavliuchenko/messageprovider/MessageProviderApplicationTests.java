4c95250852bc2ce556b8c6808487993b1155ea2d
dpavliuchenko <dpavliuchenko@ancestry.com>
2020-09-29 17:01:59 +0300

    initial project

---
+++ src/test/java/com/pavliuchenko/messageprovider/MessageProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pavliuchenko.messageprovider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageProviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
